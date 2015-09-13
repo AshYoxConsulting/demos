0cf77e51d6fd76ea489b2f22afa21ca62a414a24
ArthurRaffles <david.marc.ashton@gmail.com>
2015-09-13 12:28:18 +0100

    controller

--- simulation/src/js/simGrid/simController.js
+++ simulation/src/js/simGrid/simController.js
@@ -7,4 +7,12 @@ module.exports = function($scope) {
     'MaxLimit':850,
     'StableLimit': 450
   };
+  $scope.editing=false;
+
+  $scope.saveState=function(){
+    console.log("save to server..");
+     console.log($scope.fixedInputData);
+     $scope.editing=false;
+  }
+
 };
